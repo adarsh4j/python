93f8a7a3d882d547d55764c4518b6e0c50f0775d
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-01-17 15:42:31 +0530

    Number functions

---
+++ Number.py
@@ -0,0 +1,19 @@
+from math import *
+
+my_num=4
+
+print(my_num)
+
+print(sqrt(my_num))
+
+print(pow(my_num,2))
+
+print(str(my_num)+" is my favourite number")
+
+print(floor(3.7))
+
+print(ceil(3.7))
+
+print(min(4,5))
+
+print(max(6,9))
