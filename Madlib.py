c2508a06cfd6b15d629d647ce8406f6df5f506ef
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-01-17 19:11:54 +0530

    Create Madlib.py

---
+++ Madlib.py
@@ -0,0 +1,11 @@
+colour=input("Enter the colour : ")
+
+plural_noun=input("Enter the plural noun : ")
+
+name=input("Enter the name : ")
+
+print("Roses are "+colour)
+
+print(plural_noun+" are good")
+
+print("I love "+ name)
