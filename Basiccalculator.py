8bfa475c56ac0750856daaa423c8aabbeff9ad76
Adarsh.J <78248655+Adarsh-pgmr@users.noreply.github.com>
2022-01-17 17:32:35 +0530

    Basic calculator

---
+++ Basiccalculator.py
@@ -0,0 +1,19 @@
+num_1=input("Enter the number")
+
+num_2=input("Enter the another number")
+
+sum = float(num_1)+float(num_2)
+
+diff= float(num_1)-float(num_2)
+
+mul= float(num_1)*float(num_2)
+
+div= float(num_1)/float(num_2)
+
+print(sum)
+
+print(diff)
+
+print(mul)
+
+print(div)
