d847c213f60af84c42ceab640ee105aa239332ae
Adarsh.J <78248655+adarsh4j@users.noreply.github.com>
2023-04-02 19:28:49 +0530

    Create Pat1.py

---
+++ Latest/Pat1.py
@@ -0,0 +1,20 @@
+'''
+1 
+
+1 2 
+
+1 2 3 
+
+1 2 3 4 
+
+1 2 3 4 5'''
+
+rows = int(input("Enter the number: "))
+
+for i in range(1, rows+1):
+
+    for j in range(1, i + 1):
+
+        print(j, end=" ")
+
+    print(" ")
